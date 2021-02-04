542fea951664da648383d9022c464576e8821b54
Dawid Mszanowski <dmszanowski@icloud.com>
2021-02-04 01:34:42 +0100

    Add Contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import contactClasses from "./Contact.module.css"
 
 export class Contact extends React.Component {
     constructor(props) {
@@ -8,13 +9,23 @@ export class Contact extends React.Component {
 
     render() {
         return (
-            <div
-                style={{
-                    height: 300,
-                    backgroundColor: "#F52050"
-                }}
-            >
-            </div>
+            <React.Fragment>
+                <div className={contactClasses.contactWrapper}>
+                    <div className={contactClasses.contactForm}>
+                        <input type="text" placeholder="Name" />
+                        <input type="text" placeholder="Email" />
+                        <textarea rows={3} placeholder="Message" />
+                        <button onClick={() => console.log("will be sending smth")} className="btnLight">Send</button>
+                    </div>
+                    <div className={contactClasses.footerWrapper}>
+                        <span>
+                            2020 &copy; Dawid Mszanowski.<br />
+                            All Rights Reserved.
+                        </span>
+                    </div>
+                </div>
+            </React.Fragment>
+
         );
     }
 }
