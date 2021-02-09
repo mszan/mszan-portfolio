03b9bea76ddba2fc1f5e50b8216204dbe5868f7f
Dawid Mszanowski <dmszanowski@icloud.com>
2021-02-09 01:36:23 +0100

    Update Home animations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,31 +12,87 @@ export class Home extends React.Component {
 
     render() {
         return (
-            <div className={homeClasses.homeWrapper}>
+            <div id="home" className={homeClasses.homeWrapper}>
                 {/*background second color*/}
-                <div className={homeClasses.homeBg}/>
+                <motion.div
+                    className={homeClasses.homeBg}
+                    animate={{
+                        x: "-50vw",
+                        y: ["-200vh", "-95vh"],
+                        rotateZ: [0, 0, 0, 0, -10]
+                    }}
+                    /*transform: rotateZ(-10deg) translate(-50vw, -105vh);*/
+                    transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        duration: .5,
+                        delay: .5,
+                    }}
+                />
 
                 {/*background decoration*/}
                 <Decor />
 
-                <motion.div
-                    className={homeClasses.titleWrapper}
-                    animate={{
-                        x: ["-10vw", "0vw"],
-                        opacity: [0, 0, 1]
-                    }}
-                    transition={{ type: "spring", stiffness: 100, duration: .5 }}
-                >
-                    <div className={homeClasses.titleHello}>
+                <div className={homeClasses.titleWrapper}>
+                    <motion.div
+                        style={{
+                            fontWeight: "lighter",
+                            fontSize: 34,
+                        }}
+                        animate={{
+                            x: ["-10vw", "0vw"],
+                            opacity: [0, 0, 1]
+                        }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            duration: .5
+                        }}
+                    >
                         <span>Hello,</span>
-                    </div>
-                    <span className={homeClasses.titleIm}>I’m </span>
-                    <span
-                        className={homeClasses.titleDawid}
-                    > Dawid</span>
-                    <p className={homeClasses.titleIntro}>A 21 years old IT student looking for his first full-time <br /> job as a Junior Frontend / Web Developer</p>
+                    </motion.div>
+                    <motion.div
+                        style={{
+                            fontWeight: "normal",
+                            fontSize: 65,
+                        }}
+                        animate={{
+                            x: ["-10vw", "0vw"],
+                            opacity: [0, 0, 1]
+                        }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            duration: .5,
+                            delay: 1
+                        }}
+                    >
+                        <span>I'm</span>
+                        <span style={{fontWeight: "bold"}}>&nbsp;Dawid</span>
+                    </motion.div>
+                    <motion.div
+                        animate={{
+                            // x: ["-10vw", "0vw"],
+                            opacity: [0, 0, 1]
+                        }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 100,
+                            duration: .5,
+                            delay: 1.7
+                        }}
+                    >
+                        <p
+                            style={{
+                                color: "#8A94A7",
+                                marginBottom: "2rem"
+                            }}
+                        >
+                            A 21 years old IT student looking for his first full-time <br /> job as a Junior Frontend / Web Developer
+                        </p>
+                    </motion.div>
                     <Btns />
-                </motion.div>
+                </div>
             </div>
         );
     }
