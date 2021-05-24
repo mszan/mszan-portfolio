63670185d81579518ee5f3aabf60869da8b3c740
Dawid Mszanowski <dmszanowski@icloud.com>
2021-05-24 22:59:04 +0200

    update content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,16 +6,12 @@ export function About() {
         <div id="about" className={aboutClasses.wrapper}>
             <div className={aboutClasses.content}>
                 <h1 className={aboutClasses.title}>Dawid Mszanowski</h1>
-                <p>Hi there, I am 21 years old IT Management extramural student and I am in my second year now. In my
-                    current profession as <span>IT Specialist</span> I have developed mostly in <span>network management, systems maintenance</span> and <span>end-user support provision.</span>
+                <p>Hi there, I am 21 years old IT Management extramural student and I am in my second year now. I am currently working as a <span>Full Time Junior Web Developer</span>. In my
+                    previous profession as an <span>IT Specialist</span> I have developed mostly in <span>network management, systems maintenance</span> and <span>end-user support provision.</span>
                 </p>
-                <p>Even though, at one time or another, I work with code in my full-time job, this is not as regular and
-                    amplifying as I want it to be in the nearest future. My daily routine includes developing several
-                    applications in <span>HTML, CSS</span> and <span>SQL</span>.</p>
                 <p>My technical skills include programming in <span>JavaScript</span> and <span>Python</span> along
                     with <span>ReactJS, Django, NodeJS</span> and <span>GIT.</span><br/>I am familiar with <span>RWD, BEM, ABEM</span> and <span>SCSS</span>.
-                    I am not afraid of system’s terminal, in fact my addiction to it grows every day.<br/>I have worked
-                    in and administrated operating systems such as macOS, Ubuntu and WS.</p>
+                    <br/>I have worked in and administrated operating systems such as macOS, Ubuntu and WS.</p>
                 <br/>
                 <div className={aboutClasses.social}>
                     <a href="https://github.com/mszan">
